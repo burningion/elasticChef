3e778edee8b7ea7bee9842a42be473ecd46b09a6
KP Kaiser <kirk@zothcorp.com>
2012-10-08 17:29:17 +0000

    Added supervisord to the list of default installs.

--- cookbooks/shopplyServer/recipes/default.rb
+++ cookbooks/shopplyServer/recipes/default.rb
@@ -1,8 +1,9 @@
+include_recipe "build-essential"
 include_recipe "python::default"
 include_recipe "gunicorn::default"
 include_recipe "elasticsearch::default"
 
-%w{emacs git-core rlwrap openjdk-6-jdk tmux curl tree unzip nginx}.each do |pkg|
+%w{emacs git-core rlwrap openjdk-6-jdk tmux curl tree unzip nginx python-setuptools python-dev build-essential supervisor}.each do |pkg|
   package pkg do
     action :install
   end
@@ -16,6 +17,7 @@ service "nginx" do
 end
 
 python_virtualenv "/home/ubuntu/shopplyEnv" do
+    interpreter "python2.7"
     owner "ubuntu"
     group "ubuntu"
     action :create
